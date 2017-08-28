48a6902dd96e21cd59a2432423dfa40450255d8b
Angela Vogler <angelavogler4@gmail.com>
2017-08-28 14:30:08 -0400

    set up get routes for login, registration, home

--- app.js
+++ app.js
@@ -21,6 +21,45 @@ app.engine('mustache', mustache());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
+/* ******** REGISTRATION/LOGIN ******** */
+let members = [{
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'johndoe@test.com',
+  username: 'test',
+  password: 'abc',
+}]
+// login function
+
+// login get route
+app.get('/login', function (req, res) {
+  res.render('login');
+});
+// login post route
+
+// registration get route
+app.get('/register', function (req, res) {
+  res.render('registration');
+});
+
+// registration post route
+
+// home get route
+app.get('/home', function (req, res) {
+  res.render('home');
+});
+
+// allow you to create a snippet
+
+// allow you to see a list of all your snippets
+
+// allow you to see a list of all your snippets for a specific language
+
+// allow you to see a list of all your snippets for a specific tag
+
+// allow you to look at an individual snippet
+
+
 // start server
 app.listen(4000, function () {
   console.log('snip, snip');
---
+++ snippets.test.js
@@ -0,0 +1,7 @@
+// have a comprehensive set of tests for all controllers and models
+// have registration and login
+// allow you to create a snippet
+// allow you to see a list of all your snippets
+// allow you to see a list of all your snippets for a specific language
+// allow you to see a list of all your snippets for a specific tag
+// allow you to look at an individual snippet
