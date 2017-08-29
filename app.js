df58bc3b5f0c6f29cfac2e49d539a0dd302c76e5
Angela Vogler <angelavogler4@gmail.com>
2017-08-29 16:58:39 -0400

    got everything working with built in arrays, need to add mongoose

--- app.js
+++ app.js
@@ -21,21 +21,63 @@ app.engine('mustache', mustache());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
-/* ******** REGISTRATION/LOGIN ******** */
+// temporary storage for schemas
 let members = [{
   firstName: 'John',
   lastName: 'Doe',
-  email: 'johndoe@test.com',
   username: 'test',
   password: 'abc',
 }]
-// login function
 
+let snippets = [{
+  title: 'New Snippet',
+  code: `app.listen(4000, function () {
+        console.log('snip, snip');
+        });`,
+  notes: 'here is a snippet',
+  language: 'JavaScript',
+  tags: 'no',
+  author: 'test',
+}]
+
+/* ******** REGISTRATION/LOGIN ******** */
 // login get route
 app.get('/login', function (req, res) {
   res.render('login');
 });
 // login post route
+app.post('/login', function (req, res) {
+  // define constant variables for username and password
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // id for members
+  let member = null;
+
+  // is username valid?
+  // if so is password a match?
+  for (let i = 0; i < members.length; i++) {
+    if (username === members[i].username &&
+        password === members[i].password) {
+          member = members[i];
+        }
+    }
+
+    // if it is a match redirect them to the home screen otherwise return error message
+    if ( member !== null) {
+      // create session
+      req.session.who = member;
+
+      res.redirect('/home');
+    } else {
+      console.log('login failed');
+      console.log(username);
+      console.log(password);
+      res.render('login', {
+        loginError: true,
+      });
+    }
+});
 
 // registration get route
 app.get('/register', function (req, res) {
@@ -43,21 +85,134 @@ app.get('/register', function (req, res) {
 });
 
 // registration post route
+app.post('/register', function (req, res) {
+  const profile = req.body;
+
+  // set up session for new member
+  req.session.who = {
+    firstName: profile.firstname,
+    lastName: profile.lastname,
+    username: profile.username,
+    password: profile.password,
+  }
+
+  // push to members array
+  members.push(req.session.who);
+
+  console.log(members)
+  res.redirect('/home');
+});
 
 // home get route
 app.get('/home', function (req, res) {
-  res.render('home');
+  for (let i = 0; i < snippets.length; i++ ) {
+    if ( snippets[i].author === req.session.who.username ) {
+        res.render('home', {
+          hasSnippets: true,
+          snippets: snippets,
+        });
+    } else {
+      res.render('home', {
+        firstTime: true,
+        member: req.session.who,
+      });
+    }
+  }
 });
 
-// allow you to create a snippet
+/* ******** CREATE NEW SNIPPET ******** */
+// display form get route
+app.get('/new', function (req, res) {
+  res.render('new', {
+    snippets: snippets,
+  });
+});
 
-// allow you to see a list of all your snippets
+// post route for form
+app.post('/new', function (req, res) {
 
-// allow you to see a list of all your snippets for a specific language
+  // set up snippet
+  let snippet = ({
+    title: req.body.title,
+    code: req.body.code,
+    notes: req.body.notes,
+    language: req.body.language,
+    tags: req.body.tags,
+    author: req.session.who.username,
+  });
 
-// allow you to see a list of all your snippets for a specific tag
+  snippets.push(snippet);
+  console.log(snippets);
 
-// allow you to look at an individual snippet
+  res.render('new', {
+    newSnippet: true,
+    snippet: snippet,
+  })
+});
+
+/* ******** DISPLAY LIST OF ALL SNIPPETS ******** */
+app.get('/explore', function (req, res) {
+  res.render('display', {
+    snippets: snippets,
+  });
+});
+
+/* ******** SEE LIST OF SNIPPETS FOR LANGUAGE ******** */
+app.get('/language', function (req, res) {
+  res.render('search', {
+    languageSearch: true,
+  });
+});
+
+app.post('/language', function (req, res) {
+  let query = req.body.language_search;
+
+  for (let i = 0; i < snippets.length; i++ ) {
+    if ( query.toLowerCase() === snippets[i].language.toLowerCase() ) {
+      console.log(query);
+      console.log('success!');
+      res.render('display', {
+        snippets: snippets,
+      });
+    } else {
+      console.log(query);
+      res.send('error');
+    }
+  }
+});
+
+/* ******** SEE LIST OF SNIPPETS FOR TAG ******** */
+app.get('/tags', function (req, res) {
+  res.render('search', {
+    tagsSearch: true,
+  });
+});
+
+app.post('/tags', function (req, res) {
+  let query = req.body.tags_search;
+
+  for (let i = 0; i < snippets.length; i++ ) {
+    if ( query.toLowerCase() === snippets[i].tags.toLowerCase() ) {
+      console.log(query);
+      console.log('success!');
+      res.render('display', {
+        snippets: snippets,
+      });
+    } else {
+      console.log(query);
+      res.send('error');
+    }
+  }
+});
+
+/* ******** LOOK AT INDIVIDUAL SNIPPET ******** */
+// app.get('/title/:id', function (req, res) {
+//   req.params.id = snippets.title;
+//
+//   res.render('display', {
+//
+//   })
+// });
 
 
 // start server
--- snippets.test.js
+++ snippets.test.js
@@ -1,5 +1,30 @@
 // have a comprehensive set of tests for all controllers and models
 // have registration and login
+// login function
+function login(username, password) {
+  for (let i = 0; i < members.length; i++) {
+    // is username valid
+    if ( username === members[i].username &&
+      password === members[i].password ) {
+        member = members[i];
+      }
+
+    // is password a match?
+    if ( member !== null ) {
+      // create the session
+      req.session.who = member;
+
+      // and redirect the user home
+      res.redirect('/home');
+    } else {
+      // direct the user back to login with error message
+      res.render('login', {
+        loginError: true,
+      });
+    }
+  }
+}
+
 // allow you to create a snippet
 // allow you to see a list of all your snippets
 // allow you to see a list of all your snippets for a specific language
